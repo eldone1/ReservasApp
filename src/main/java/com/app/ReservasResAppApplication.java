43c932dfd98253aae4d91b3087963692213a23a4
Diego M. <diego.dme966@gmail.com>
2026-03-21 12:35:06 -0500

    Initial commit

---
+++ src/main/java/com/app/ReservasResAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasResAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasResAppApplication.class, args);
+	}
+
+}
